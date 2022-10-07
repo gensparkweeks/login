17b65dc55de3842587f39679fecf7904bd8cee0b
Minardo Toirac Isla <gensparkweeks@gmail.com>
2022-10-06 22:25:07 -0400

    adding controller/service

--- login-back/src/main/java/com/sql/loginback/controller/UserController.java
+++ login-back/src/main/java/com/sql/loginback/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.sql.loginback.controller;
 
+import com.sql.loginback.model.User;
 import com.sql.loginback.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/user")
 public class UserController {
 
     @Autowired
@@ -16,5 +17,30 @@ public class UserController {
     @GetMapping("/home")
     public String home(){
         return "<html><h1>Welcome to my Login page SpringBoot/SQL App</h1></html>";
+    }
+
+    @GetMapping
+    public List<User> findAll(){
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable int id){
+        return userService.findById(id);
+    }
+
+    @PostMapping
+    public User save(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user){
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        userService.deleteUser(id);
     }
 }
--- login-back/src/main/java/com/sql/loginback/service/UserServiceImpl.java
+++ login-back/src/main/java/com/sql/loginback/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.sql.loginback.service;
 import com.sql.loginback.dao.UserDao;
 import com.sql.loginback.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +12,9 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
 
     @Override
     public List<User> findAll() {
@@ -24,11 +28,17 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User createUser(User user) {
+        if (user.getPassword() != null){
+            user.setPassword(encoder.encode(user.getPassword()));
+        }
         return userDao.save(user);
     }
 
     @Override
     public User updateUser(User user) {
+        if (user.getPassword() != null){
+            user.setPassword(encoder.encode(user.getPassword()));
+        }
         return userDao.save(user);
     }
 
